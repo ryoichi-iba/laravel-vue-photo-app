0ee523559af6ec21984b82b4e4f202b86b1af56c
Ryoichi Iba <ryoichiaz18@gmail.com>
2021-02-13 14:10:00 +0900

    add axios

--- web/resources/js/bootstrap.js
+++ web/resources/js/bootstrap.js
@@ -1,3 +1,4 @@
+import { getCookieValue } from './util'
 window._ = require('lodash');
 
 /**
@@ -8,7 +9,15 @@ window._ = require('lodash');
 
 window.axios = require('axios');
 
+// Ajaxリクエストであることを示すヘッダーを付与する
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+window.axios.interceptors.request.use(config => {
+  // クッキーからトークンを取り出してヘッダーに添付する
+  config.headers['X-XSRF-TOKEN'] = getCookieValue('XSRF-TOKEN')
+
+  return config
+})
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
