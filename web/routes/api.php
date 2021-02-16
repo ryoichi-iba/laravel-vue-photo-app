4e1c3100ee690ec2173a48fc69b74903042443b8
Ryoichi Iba <ryoichiaz18@gmail.com>
2021-02-16 17:08:50 +0900

    add s3

--- web/routes/api.php
+++ web/routes/api.php
@@ -12,3 +12,5 @@ Route::post('/login', 'Auth\LoginController@login')->name('login');
 Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
 
 Route::get('/user', fn () => Auth::user())->name('user');
+
+Route::post('/photos', 'PhotoController@create')->name('photo.create');
